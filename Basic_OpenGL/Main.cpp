772e0934e9d9ba4cda9103fa84e3ff30c0d61086
Skelkingr <aymeric.goupil@gmail.com>
2023-01-17 18:10:36 +0100

    Create window.

--- Basic_OpenGL/Main.cpp
+++ Basic_OpenGL/Main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
 
+#include <Windows.h>
+
+#include <SDL.h>
+#include <gl/GL.h>
+
+bool isAppRunning = true;
+SDL_Window* window = nullptr;
+SDL_GLContext context = nullptr;
+
 int main(int argc, char* argv[])
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		std::cout << "[ERR] Error initializing SDL." << std::endl;
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);	
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+
+	window = SDL_CreateWindow("Skelkingr", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
+
+	if (!window)
+	{
+		std::cout << "[ERR] Error creating SDL window." << std::endl;
+	}
+
+	context = SDL_GL_CreateContext(window);
+
+	if (!context) 
+	{
+		std::cout << "[ERR] Error creating OpenGL context." << std::endl;
+	}
+
+	while (isAppRunning)
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+
+	system("pause");
 	return 0;
 }
